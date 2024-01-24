041cb1bbc837bac0841f65ef03a762709946f104
Al_P <al.e.poliakow@gmail.com>
2024-01-24 20:15:29 +1100

    Display word and guessed letters with comments

--- js/script.js
+++ js/script.js
@@ -34,7 +34,7 @@ button.addEventListener("click", function(e){
     const validatedGuess= validateInput(guess);
    // console.log(validatedGuess);
    letterInput.value="";
-   makeGuess(guess);
+   makeGuess(validatedGuess); //if guess is used; validateInput is bypassed
 });
 
 const validateInput = function(input) {
@@ -55,10 +55,60 @@ const validateInput = function(input) {
 // function that accepts a letter as the parameter
 const makeGuess = function(guess){
     guess=guess.toUpperCase(); // convert to upper case for JavaScript
- if (guessedLetters.includes(guess)){
+
+    if (guessedLetters.includes(guess)){
     message.innerText='You have already guessed that letter, try a different letter';
- } else {
+    } else {
     guessedLetters.push(guess);
     console.log(guessedLetters);
- }
+    displayGuessedLetters(guess);
+    updateWordInProgress(guessedLetters);
+    }
 };
+
+//create a function to show the guessed letters
+const displayGuessedLetters = function(){
+    guessedLettersElement.innerHTML="";
+    // create a new list item for each letter inside the guessedLetters array and add it to the unordered list
+    for (const letter of guessedLetters){
+    const li = document.createElement("li");
+    li.innerText = letter;
+    guessedLettersElement.append(li);
+    }
+};
+
+const updateWordInProgress = function (guessedLetters){
+    //change letters to uppercase
+    const wordUpper = word.toUpperCase();
+    //split the word string into an array so that the letter can appear in the guessedLetters array
+    const wordArray = wordUpper.split("");
+    //check if it works
+    console.log(wordArray);
+    //create a variable/array to show the correctly guessed letters (replaces placeholder)
+    const revealLetter = [];
+    for (const letter of wordArray){
+        //if the guess is included
+        if (guessedLetters.includes(letter)){
+            revealLetter.push(letter.toUpperCase());
+            //if the guess is not included
+        } else {
+            revealLetter.push("●");
+        }
+    }
+    //check if it works
+    //console.log(revealLetter);
+    //update the circle symbol with the correct letter
+    wordInProgress.innerText = revealLetter.join("");
+    checkForWin();
+};
+
+const checkForWin = function () { 
+    //if the word in progress matches the word to guess
+    if (wordInProgress.innerText === word.toUpperCase()) {
+        //update message class to add win
+        message.classList.add("win");
+        //update message contents
+        message.innerHTML = `<p class="highlight">You guessed correct the word! Congrats!</p>`;
+    }
+};
+
